c9040a4347e4d465061cefb9ee0b0c254b99faa6
Matt Shen <lovelywib@gmail.com>
2023-12-26 16:24:08 +1100

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- closing
+vim.keymap.set("n", "<leader>x", "<cmd>x<cr>", { desc = "Close Window" })
+
+-- Search words, texts, etc
+vim.keymap.set("n", "<leader>ps", "<cmd>grep <cword><cr><cmd>copen<cr>", { desc = "Grep current word" })
+
+-- telescope custom
+vim.keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "View All Marks" })
