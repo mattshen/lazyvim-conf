df6cf2d6a73fe28c0def86a6ca00bf234a6c972b
Matt Shen <lovelywib@gmail.com>
2023-12-26 16:25:48 +1100

    Create terminal.lua

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,22 @@
+if true return {}
+
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+      })
+    end,
+    keys = {
+      { [[<C-\>]] },
+      { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+      {
+        "<leader>td",
+        "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+    },
+  },
+}
